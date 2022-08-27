5c7a772b5aeb5f915ec1eff736fdeaf6d0bdda07
Omolara5861 <debbiegterra@gmail.com>
2022-08-27 05:25:44 +0100

    complete hambuger style

--- js/script.js
+++ js/script.js
@@ -2,7 +2,9 @@ const btn = document.getElementById('hamburger')
 const nav = document.getElementById('menu')
 
 btn.addEventListener('click', () => {
-    btn.classList.toggle('open')
+    nav.classList.toggle('mobile-menu-bg');
+    nav.classList.toggle('mt-4');
+
     nav.classList.toggle('flex')
     nav.classList.toggle('hidden')
 })
